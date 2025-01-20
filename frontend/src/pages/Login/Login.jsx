de04b5526130d96599efca784b979474e9ef1bb1
imjayram <126317957+imjayram@users.noreply.github.com>
2025-01-20 23:25:09 +0530

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -40,7 +40,7 @@ const Login = () => {
       dispatch(signInStart())
 
       const res = await axios.post(
-        "http://localhost:3000/api/auth/login",
+        "https://note-app-1ias.vercel.app/api/auth/login",
         { email, password },
         { withCredentials: true }
       )
